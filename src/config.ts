f53561a32f6fa52ba9142ff42695aa1b0c39bf00
0xRaynor <raynorli@proton.me>
2023-07-28 12:10:05 +0700

    add back links

--- src/config.ts
+++ src/config.ts
@@ -15,25 +15,20 @@ export const routeConfigs: {
   {
     path: "/earn",
     name: "Earn"
+  },
+  {
+    path: "https://medium.com/@Adscendo/weedpaper-of-adscendo-773eb7356bad",
+    name: "WeedPaper",
+    external: true
+  },
+  {
+    path: "https://medium.com/@Adscendo",
+    name: "Medium",
+    external: true
+  },
+  {
+    path: "https://docs.adscendo.xyz/",
+    name: "Docs",
+    external: true
   }
-  // {
-  //   path: "https://medium.com/@Adscendo/weedpaper-of-adscendo-773eb7356bad",
-  //   name: "WeedPaper",
-  //   external: true
-  // },
-  // {
-  //   path: "https://medium.com/@Adscendo",
-  //   name: "Medium",
-  //   external: true
-  // },
-  // {
-  //   path: "https://docs.adscendo.xyz/",
-  //   name: "Docs",
-  //   external: true
-  // },
-  // {
-  //   path: "/public-sale",
-  //   name: "Public Sale",
-  //   disabled: true
-  // }
 ];

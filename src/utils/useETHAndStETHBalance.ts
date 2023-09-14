3a88b130b68836bcba679c8d4df5df16e875f58b
0xRaynor <raynorli@proton.me>
2023-09-14 11:29:23 +0700

    update contract

--- src/utils/useETHAndStETHBalance.ts
+++ src/utils/useETHAndStETHBalance.ts
@@ -1,6 +1,7 @@
 import { useAccount, useBalance } from "wagmi";
 import { CONTRACT_ADDRESSES } from "@src/constants.ts";
 import useTabFocused from "@hooks/utils/useTabFocused.ts";
+import { isPublicSalePage } from "@src/config.tsx";
 
 const useETHAndStETHBalance = () => {
   const { address } = useAccount();
@@ -14,7 +15,9 @@ const useETHAndStETHBalance = () => {
 
   const { data: stETHBalanceData } = useBalance({
     address,
-    token: CONTRACT_ADDRESSES.stETH,
+    token: isPublicSalePage
+      ? "0xae7ab96520de3a18e5e111b5eaab095312d7fe84"
+      : CONTRACT_ADDRESSES.stETH,
     watch: focused
   });
 

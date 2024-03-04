e6c13ba2c73cccb96f36fc7d7de1020bc110b513
Danus3 <cl15213298081@gmail.com>
2024-03-04 23:33:24 +0800

    feat:update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
   resolve: {
     alias: {
@@ -14,7 +15,7 @@ export default defineConfig({
       "@utils": path.resolve(__dirname, "./src/utils"),
       "@hooks": path.resolve(__dirname, "./src/hooks"),
       "@state": path.resolve(__dirname, "./src/state"),
-      "@pages": path.resolve(__dirname, "./src/pages")
-    }
-  }
+      "@pages": path.resolve(__dirname, "./src/pages"),
+    },
+  },
 });
